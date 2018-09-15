7e26b9dfb0a582a4fda9aeb0d02daf33d7295d8b
GustavoAgt <gustavo.adolf.gomez@gmail.com>
2018-09-15 17:29:44 -0400

    createNewBlock completed and its basic test

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -3,7 +3,20 @@ function Blockchain() {
     this.newTransactions = [];
 }
 
-Blockchain.prototype.createNewBlock= function (nonce, previousBlockHash, hash) {
-    
+Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
+    const newBlock = {
+        index: this.chain.length + 1,
+        timestamp: Date.now(),
+        transactions: this.newTransactions,
+        nonce: nonce,
+        hash: hash,
+        previousBlockHash: previousBlockHash
+    };
+
+    this.newTransactions = [];
+    this.chain.push(newBlock);
+
+    return newBlock;
 }
 
+module.exports = Blockchain;
---
+++ dev/test.js
@@ -0,0 +1,5 @@
+const blockChain = require('./blockchain');
+
+const bitcoin = new  blockChain();
+bitcoin.createNewBlock(44425, 'DSDDS877887SDSD87', '90SADASDIIIWEJKA4545A54SD');
+console.log(bitcoin);
