5370843b8b1864aba2c0de9869cd614bfd59e3d4
GustavoAgt <gustavo.adolf.gomez@gmail.com>
2018-09-22 08:08:00 -0400

    nodemon cofig added to express

--- dev/api.js
+++ dev/api.js
@@ -1,17 +1,28 @@
 var express = require('express');
 var app = express();
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
 app.get('/blockchain', function (req, res) {
-  
+    
 });
 
 app.post('/transaction', function (req, res) {
-
+    console.log(req.body);
+    res.send(`The amout of the transaction is ${req.body.amount}`);
 });
 
 app.get('/mine', function (req, res) {
 
 });
-app.listen(3000, function(){
+
+
+app.listen(3000, function () {
     console.log('Listening port 3000...');
 });
